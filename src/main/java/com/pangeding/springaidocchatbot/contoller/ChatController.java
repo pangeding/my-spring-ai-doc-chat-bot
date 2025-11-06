02c9250a35fa07edd60de3f107a09231fb7d07b8
pangeding <15533858+pangeding@user.noreply.gitee.com>
2025-11-06 16:46:46 +0800

    utf8显示 流式

--- src/main/java/com/pangeding/springaidocchatbot/contoller/ChatController.java
+++ src/main/java/com/pangeding/springaidocchatbot/contoller/ChatController.java
@@ -1,11 +1,15 @@
 package com.pangeding.springaidocchatbot.contoller;
 
 
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +23,7 @@ import java.util.Map;
  * @author @debug.icu
  * @date 2024-03-10 17:05
  */
+@Slf4j
 @RestController
 public class ChatController {
 
@@ -32,14 +37,18 @@ public class ChatController {
     @GetMapping("/ai/generate")
     public Map<String, String> generate(@RequestParam(value = "message", defaultValue = "介绍你自己") String message) {
         String response = chatClient.prompt().user(message).call().content();
+        log.info("User message: {}, AI response: {}", message, response);
         return Map.of("generation", response);
     }
 
-    @GetMapping("/ai/generateStream")
-    public Flux<String> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+    @GetMapping(value = "/ai/generateStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE + ";charset=UTF-8")
+    public Flux<String> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message, HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
+        log.info("User message (streaming): {}", message);
         return chatClient.prompt()
                 .user(message)
                 .stream()
                 .content();
+
     }
 }
